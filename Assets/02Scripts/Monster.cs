d1b0e68fddbd997369b7af5201431d231d4d8dcc
PanDaJoa <jms3590743@gmail.com>
2024-03-05 18:18:25 +0900

    [전민성] 몬스터 일정거리에 들어가면 쫒아오는거 구현

--- Assets/02Scripts/Monster.cs
+++ Assets/02Scripts/Monster.cs
@@ -2,49 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public enum MonsterState
 {
     Idle,
     Trace,
-    Comeback,
-    Attack
-}
-public class monster : MonoBehaviour
-{
-    private Transform _target;
-    public float MoveSpeed = 5;          // 몬스터 속도
-    public float FindDistance = 5f;      // 감지거리
-    public Vector3 StartPosition;        // 시작위치
-    public float MoveDistance = 40f;     // 움직일 수 있는 거리
-    public const float TOLERANCE = 0.1f; // 플레이어와 몬스터간의 거리
-    public float AttackDistance = 2f;    // 공격범위
-    public float Damage = 10;
-    private float _attackTimer = 0f;     // 공격타임
-    private float _idleTimer;
+    Comeback
 
+}
+
+public class Monster : MonoBehaviour
+{
+    private Transform _target;            // 
+    public float MoveSpeed       = 5;     // 몬스터 속도
+    public float FindDistance    = 5f;    // 감지거리
+    public Vector3 StartPosition;         // 시작위치
+    public float MoveDistance    = 40f;   // 움직일 수 있는 거리
+    public const float TOLERANCE = 0.1f;  // 플레이어와 몬스터간의 거리
+    public float AttackDistance  = 2f;    // 공격범위
+    public float Damage          = 10;
+    private float _attackTimer   = 0f;    // 공격타임
+    public const float AttackDelay = 1f;  // 공격딜레이
+    private float _idleTimer;
 
     private NavMeshAgent _navMeshAgent;
     private Animator _animator;
 
     private MonsterState _currentState = MonsterState.Idle;
-    public void Start()
+    private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
-        _navMeshAgent.speed = MoveSpeed;
-
         _animator = GetComponentInChildren<Animator>();
 
-        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        _target = GameObject.FindGameObjectWithTag("Player").transform; // 타겟에다가 태그 플레이어를 넣어줌
 
         StartPosition = transform.position;
-
-        Init();
-    }
-
-    public void Init()
-    {
-        _idleTimer = 0f;
     }
 
     public void Update()
@@ -54,26 +45,13 @@ public class monster : MonoBehaviour
             case MonsterState.Idle:
                 Idle();
                 break;
-
             case MonsterState.Trace:
                 Trace();
                 break;
-
-            case MonsterState.Comeback:
-                Comeback();
-                break;
-
-            case MonsterState.Attack:
-                Attack();
-                break;
         }
     }
-
     public void Idle()
     {
-        _idleTimer += Time.deltaTime;
-
-        // todo: 몬스터의 Idle 애니메이션 재생
         if (Vector3.Distance(_target.position, transform.position) <= FindDistance)
         {
             Debug.Log("상태 전환: Idle -> Trace");
@@ -84,72 +62,18 @@ public class monster : MonoBehaviour
 
     public void Trace()
     {
-        Vector3 dir = _target.transform.position - this.transform.position;
-        dir = nomal
+        Vector3 dir = _target.position - this.transform.position;
+        dir.Normalize();
 
+        // 내비게이션 목적지를 타겟으로 위치
         _navMeshAgent.destination = _target.position;
 
-        if (Vector3.Distance(transform.position, StartPosition) >= MoveDistance)
+        if(Vector3.Distance(_target.position, StartPosition) >= MoveDistance)
         {
             Debug.Log("상태 전환: Trace -> Comeback");
             _animator.SetTrigger("TraceToComeback");
             _currentState = MonsterState.Comeback;
         }
-    }
-    private void Comeback()
-    {
-        // 실습 과제 34. 복귀 상태의 행동 구현하기:
-        // 시작 지점 쳐다보면서 시작지점으로 이동하기 (이동 완료하면 다시 Idle 상태로 전환)
-        // 1. 방향을 구한다. (target - me)
-        Vector3 dir = StartPosition - this.transform.position;
-        dir.y = 0;
-        dir.Normalize();
-        // 2. 이동한다.
-        //_characterController.Move(dir * MoveSpeed * Time.deltaTime);
-        // 3. 쳐다본다.
-        //transform.forward = dir; //(_target);
-
-        // 내비게이션이 접근하는 최소 거리를 오차 범위
-        _navMeshAgent.stoppingDistance = TOLERANCE;
-
-        // 내비게이션의 목적지를 플레이어의 위치로 한다.
-        _navMeshAgent.destination = StartPosition;
-
-        if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= TOLERANCE)
-        {
-            Debug.Log("상태 전환: Comeback -> idle");
-            _animator.SetTrigger("ComebackToIdle");
-            _currentState = MonsterState.Idle;
-        }
-
-        if (Vector3.Distance(StartPosition, transform.position) <= TOLERANCE)
-        {
-            Debug.Log("상태 전환: Comeback -> idle");
-            _animator.SetTrigger("ComebackToIdle");
-            _currentState = MonsterState.Idle;
-        }
-
-    }
-
-    private void Attack()
-    {
-        // 전이 사건: 플레이어와 거리가 공격 범위보다 멀어지면 다시 Trace
-        if (Vector3.Distance(_target.position, transform.position) > AttackDistance)
-        {
-            _attackTimer = 0f;
-            Debug.Log("상태 전환: Attack -> Trace");
-            _animator.SetTrigger("AttackToTrace");
-            _currentState = MonsterState.Trace;
-            return;
-        }
-
- /*       // 실습 과제 35. Attack 상태일 때 N초에 한 번 때리게 딜레이 주기
-        _attackTimer += Time.deltaTime;
-        if (_attackTimer >= AttackDelay)
-        {
-            _animator.SetTrigger("Attack");
-        }*/
-
     }
 }
 
