b1abe7f55db834e4826d270b9fec58d9031eb50d
PanDaJoa <jms3590743@gmail.com>
2024-03-05 20:01:48 +0900

    comeback 코드 약간수정

--- Assets/02Scripts/Monster.cs
+++ Assets/02Scripts/Monster.cs
@@ -12,12 +12,12 @@ public enum MonsterState
 
 public class Monster : MonoBehaviour
 {
-    private Transform _target;            // 
+    private Transform _target;            // 플레이어
     public float MoveSpeed       = 5;     // 몬스터 속도
     public float FindDistance    = 5f;    // 감지거리
     public Vector3 StartPosition;         // 시작위치
     public float MoveDistance    = 40f;   // 움직일 수 있는 거리
-    public const float TOLERANCE = 0.1f;  // 플레이어와 몬스터간의 거리
+    public const float TOLERANCE = 0.1f;  // 오차범위
     public float AttackDistance  = 2f;    // 공격범위
     public float Damage          = 10;
     private float _attackTimer   = 0f;    // 공격타임
@@ -31,11 +31,20 @@ public class Monster : MonoBehaviour
     private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.speed = MoveSpeed;
+
         _animator = GetComponentInChildren<Animator>();
 
         _target = GameObject.FindGameObjectWithTag("Player").transform; // 타겟에다가 태그 플레이어를 넣어줌
 
         StartPosition = transform.position;
+
+        Init();
+    }
+
+    public void Init()
+    {
+        _idleTimer = 0f;
     }
 
     public void Update()
@@ -48,10 +57,14 @@ public class Monster : MonoBehaviour
             case MonsterState.Trace:
                 Trace();
                 break;
+            case MonsterState.Comeback:
+                Comeback();
+                break;
         }
     }
     public void Idle()
     {
+        _idleTimer += Time.deltaTime;
         if (Vector3.Distance(_target.position, transform.position) <= FindDistance)
         {
             Debug.Log("상태 전환: Idle -> Trace");
@@ -63,20 +76,47 @@ public class Monster : MonoBehaviour
     public void Trace()
     {
 
-        Vector3 dir = _target.position - this.transform.position;
-        dir.Normalize();
         Vector3 dir = _target.transform.position - this.transform.position;
-
-
+        dir.Normalize();
+       
         // 내비게이션 목적지를 타겟으로 위치
         _navMeshAgent.destination = _target.position;
 
-        if(Vector3.Distance(_target.position, StartPosition) >= MoveDistance)
+        if (Vector3.Distance(transform.position, StartPosition) >= MoveDistance)
         {
             Debug.Log("상태 전환: Trace -> Comeback");
             _animator.SetTrigger("TraceToComeback");
             _currentState = MonsterState.Comeback;
         }
+
+    }
+    public void Comeback()
+    {
+        Vector3 dir = StartPosition - this.transform.position;
+        dir.Normalize();
+
+        // 내비게이션이 접근하는 최소 거리를 오차 범위
+        _navMeshAgent.stoppingDistance = TOLERANCE;
+
+        // 내비게이션 목적지를 스타트 지점으로
+        _navMeshAgent.destination = StartPosition;
+
+        if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= TOLERANCE)
+        {
+            Debug.Log("상태 전환: Comeback -> idle");
+            _animator.SetTrigger("ComebackToIdle");
+            _currentState = MonsterState.Idle;
+        }
+
+        if (Vector3.Distance(StartPosition, transform.position) <= TOLERANCE)
+        {
+            Debug.Log("상태 전환: Comeback -> idle");
+            _animator.SetTrigger("ComebackToIdle");
+            _currentState = MonsterState.Idle;
+        }
+
     }
 }
+
+
 
