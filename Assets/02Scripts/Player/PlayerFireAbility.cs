e2c828d31b48e497ade079ae33090cd3b9114a19
조희수 <hs.jo.sw.p@gmail.com>
2024-03-08 14:24:03 +0900

    [조희수] 어택

--- Assets/02Scripts/Player/PlayerFireAbility.cs
+++ Assets/02Scripts/Player/PlayerFireAbility.cs
@@ -6,7 +6,7 @@ public class PlayerFireAbility : MonoBehaviour
     public GameObject EquipmentPrefab;
     public Transform SwingPosition;
     public int Damage = 10; // 공격력
-    public float AttackRange = 2f; // 공격 범위
+    public float AttackRange = 10f; // 공격 범위
     private GameObject equipmentInstance; // 현재 활성화된 무기 인스턴스를 추적
     private bool isEquipped = false; // 무기 장착 상태를 판단하는 변수
 
@@ -40,16 +40,17 @@ public class PlayerFireAbility : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButton(0) && _animator != null)
+        if (Input.GetMouseButtonDown(0) && _animator != null)
         {
             _animator.SetTrigger("AxeHit");
             AttackIfInRange();
-            // + 무기 휘두르는 effect에다가 콜라이더 입혀서 범위 넓게 타격 입히도록 구현(raycast 별개임 물체 검사 함수)
-            //도끼 휘두를 때 타격 데미지 Raycast도 하고 + 상자 콜라이더 인식해서 E키 누르면 잡을 수 있도록
+           
         }
 
     }
 
+    // + 무기 휘두르는 effect에다가 콜라이더 입혀서 범위 넓게 타격 입히도록 구현(raycast 별개임 물체 검사 함수)
+    //도끼 휘두를 때 타격 데미지 Raycast도 하고 + 상자 콜라이더 인식해서 E키 누르면 잡을 수 있도록
     void AttackIfInRange()
     {
         // Raycast를 사용하여 공격 범위 안에 있는 몬스터를 탐지
@@ -64,7 +65,7 @@ public class PlayerFireAbility : MonoBehaviour
             {
                 DamageInfo damageInfo = new DamageInfo(DamageType.Normal, Damage);
                 monster.Hit(damageInfo);
-                Debug.Log("hIT");
+                
             }
         }
     }
