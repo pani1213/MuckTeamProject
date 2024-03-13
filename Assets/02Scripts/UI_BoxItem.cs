1ca6f9b4854c119b49ad20552429b9ebb4f4d586
조희수 <hs.jo.sw.p@gmail.com>
2024-03-13 13:02:30 +0900

    [조희수] 랜덤아이템 UI 백그라운드 크기 조절

--- Assets/02Scripts/UI_BoxItem.cs
+++ Assets/02Scripts/UI_BoxItem.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_BoxItem : MonoBehaviour
 {
+    public int maxColumns = 2; // 최대 열의 수
+    public int maxRows = 5; // 최대 행의 수
+
     // 박스 E키 눌렀을 때, Creating item of type: 'jumpPower'에 따라 UI 생성
     public GameObject backgroundImage;
-    public TextMeshPro CountText;
+
+    public TextMeshProUGUI AvocadoCountText;
+    public TextMeshProUGUI BananaCountText;
+    public TextMeshProUGUI BreadCountText;
+    public TextMeshProUGUI BroccoliCountText;
+    public TextMeshProUGUI CarrotCountText;
+    public TextMeshProUGUI FishCountText;
+    public TextMeshProUGUI GarlicCountText;
+    public TextMeshProUGUI PearCountText;
+    public TextMeshProUGUI PepperCountText;
+    public TextMeshProUGUI PumpkinCountText;
 
     private Dictionary<BoxItemType, GameObject> itemImages = new Dictionary<BoxItemType, GameObject>();
     private Dictionary<BoxItemType, int> itemCounts = new Dictionary<BoxItemType, int>();
@@ -23,9 +40,24 @@ public class UI_BoxItem : MonoBehaviour
     public GameObject PepperImage;
     public GameObject PumpkinImage;
 
+    private GridLayoutGroup gridLayoutGroup;
+
     private void Awake()
     {
         backgroundImage.SetActive(false);
+
+        gridLayoutGroup = backgroundImage.GetComponent<GridLayoutGroup>();
+        if (gridLayoutGroup == null)
+        {
+            gridLayoutGroup = backgroundImage.AddComponent<GridLayoutGroup>();
+        }
+
+        // GridLayoutGroup 속성 설정
+        gridLayoutGroup.cellSize = new Vector2(50, 50); // 적절한 셀 크기 설정
+        gridLayoutGroup.spacing = new Vector2(3, 3); // 적절한 간격 설정
+        gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayoutGroup.constraintCount = maxColumns; // 최대 열 수 설정
+
 
         itemImages[BoxItemType.maxhp] = AvocadoImage;
         itemImages[BoxItemType.stamina] = BreadImage;
@@ -43,7 +75,7 @@ public class UI_BoxItem : MonoBehaviour
             image.SetActive(false);
         }
     }
-
+   
     public void ShowBoxItem(BoxItemType itemType)
     {
         // 아이템 개수 업데이트
@@ -55,26 +87,61 @@ public class UI_BoxItem : MonoBehaviour
         itemCounts[itemType]++;
 
         // UI 업데이트
-        Refresh(itemType);
+        Refresh();
     }
 
     // UI를 새로고침 하는 함수
-    public void Refresh(BoxItemType itemType)
+    public void Refresh()
     {
+        // 모든 아이템 이미지와 개수를 숨깁니다.
         foreach (var image in itemImages.Values)
         {
-            image.SetActive(false);
+            image.SetActive(false); // 일단 모든 아이템 이미지를 숨김
         }
 
-        if (itemImages.ContainsKey(itemType))
+        // 현재 가지고 있는 모든 아이템 타입에 대해 반복하여 처리
+        foreach (var itemType in itemCounts.Keys)
         {
-            // foreach 문 써서 그동안 먹은 아이템 다 띄우는 코드
-            itemImages[itemType].SetActive(true);
-            Debug.Log("UI를 새로고침");
+            // 해당 아이템 타입에 맞는 이미지가 있으면 활성화하고 개수를 업데이트
+            if (itemImages.ContainsKey(itemType))
+            {
+                itemImages[itemType].SetActive(true); // 아이템 이미지 활성화
+
+                // 개별 아이템 이미지 옆에 개수를 표시할 TextMeshProUGUI 컴포넌트를 찾아서 업데이트
+                var countText = itemImages[itemType].GetComponentInChildren<TextMeshProUGUI>(true);
+                if (countText != null)
+                {
+                    countText.text = itemCounts[itemType].ToString(); // 아이템 개수 업데이트
+                    countText.gameObject.SetActive(true); // 개수를 표시하는 텍스트 활성화
+                }
+            }
         }
-
         backgroundImage.SetActive(true);
+        AdjustBackgroundSize();
+    }
 
-            //CountText.text = $"{itemType.ToString()}: {itemCounts[itemType]}";
+    // backgroundImage 크기 조절 함수
+    private void AdjustBackgroundSize()
+    {
+        // 이미지가 활성화된(실제로 표시되는) 아이템의 개수를 카운트
+        int activeItemCount = 0;
+        foreach (var image in itemImages.Values)
+        {
+            if (image.activeSelf)
+            {
+                activeItemCount++;
+            }
+        }
+        // 필요한 총 행과 열 계산
+        int totalColumnsNeeded = Mathf.Min(activeItemCount, maxColumns);
+        int totalRowsNeeded = Mathf.CeilToInt((float)activeItemCount / maxColumns);
+
+        // 배경의 너비와 높이를 조절합니다.
+        float width = totalColumnsNeeded * gridLayoutGroup.cellSize.x + (totalColumnsNeeded - 1) * gridLayoutGroup.spacing.x + gridLayoutGroup.padding.left + gridLayoutGroup.padding.right;
+        float height = totalRowsNeeded * gridLayoutGroup.cellSize.y + (totalRowsNeeded - 1) * gridLayoutGroup.spacing.y + gridLayoutGroup.padding.top + gridLayoutGroup.padding.bottom;
+
+        // 조절된 너비와 높이로 backgroundImage의 크기를 설정
+        var rectTransform = backgroundImage.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(width, height);
     }
 }
