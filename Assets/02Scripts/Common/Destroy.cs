a237c7d05044ca5e15809fb69247c27b24663ba5
Jeremy <toddlf091194@gmail.com>
2024-03-16 15:21:33 +0900

    [정성훈] 씬 병합 완료

--- Assets/02Scripts/Common/Destroy.cs
+++ Assets/02Scripts/Common/Destroy.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
-    public GameObject[] DestroyGround; // 게임오브젝트 그라운드
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Monster"))
