760eb2320a77c1726e03761cd884dc7c22f0a8cf
PanDaJoa <jms3590743@gmail.com>
2024-03-18 12:37:50 +0900

    [전민성] 허기, 스태미나도 100넘어도 되게 수정

--- Assets/PlayerStateController.cs
+++ Assets/PlayerStateController.cs
@@ -19,12 +19,22 @@ public class PlayerStateController : MonoBehaviour
 
     public void SetHungry()
     {
-        hungry.fillAmount = SurvivalGauge.Instance.PlayerHunger * 0.01f;
+        float PlayerHunger = SurvivalGauge.Instance.PlayerHunger;
+        float maxHunger = SurvivalGauge.Instance.Maxhunger;
+
+        float normalizedHunger= Mathf.Clamp(PlayerHunger / maxHunger, 0f, 1f);
+
+        hungry.fillAmount = normalizedHunger;
     }
 
     public void SetStamina()
     {
-        stamina.fillAmount = SurvivalGauge.Instance.Stamina * 0.01f;
+        float PlayerStamina = SurvivalGauge.Instance.Stamina;
+        float maxStamina = SurvivalGauge.Instance.MaxStamina;
+
+        float normalizedStamina = Mathf.Clamp(PlayerStamina / maxStamina, 0f, 1f);
+
+        stamina.fillAmount = normalizedStamina;
     }
 
     private void Update()
