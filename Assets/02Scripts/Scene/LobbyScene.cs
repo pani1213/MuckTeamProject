c210004bc81cb8a3987dbf5aaf519953ef21bb0a
PanDaJoa <jms3590743@gmail.com>
2024-03-19 12:31:32 +0900

    [전민성] 스타트씬에서 메인씬으로 가기 구현

---
+++ Assets/02Scripts/Scene/LobbyScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SecneNames
+{
+    Lobby,  // 0
+    Main    // 1
+}
+public class LobbyScene : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        Debug.Log("게임시작");
+        SceneManager.LoadScene((int)SecneNames.Main);
+    }
+}
